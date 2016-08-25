329035ad1569f3a28c8674767fa552944fcff87b
changjiashuai <changjiashuai@gmail.com>
2016-08-25 11:11:49 +0800

    init project

---
+++ pulltorefresh/src/main/java/io/github/changjiashuai/pulltorefreshlayout/PullToRefreshLayout.java
@@ -0,0 +1,32 @@
+package io.github.changjiashuai.pulltorefreshlayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Email: changjiashuai@gmail.com
+ *
+ * Created by CJS on 16/8/25 11:05.
+ */
+public class PullToRefreshLayout extends FrameLayout {
+
+    public PullToRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public PullToRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullToRefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public PullToRefreshLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
